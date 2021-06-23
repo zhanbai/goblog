1bf1e32fb8ffb494a08179329ed756c7a8b30a61
zhanbai <bai.zhan@qq.com>
2021-06-23 20:41:26 +0800

    feat:优化中间件结构和使用

--- main.go
+++ main.go
@@ -2,41 +2,17 @@ package main
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/zhanbai/goblog/app/http/middlewares"
 	"github.com/zhanbai/goblog/pkg/route"
 )
 
 var router *mux.Router
 
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 除首页以外，移除所有请求路径后面的斜杠
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		// 2. 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	route.Initialize()
 	router = route.Router
 
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
--- routes/web.go
+++ routes/web.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/zhanbai/goblog/app/http/controllers"
+	"github.com/zhanbai/goblog/app/http/middlewares"
 )
 
 // RegisterWebRoutes 注册网页相关路由
@@ -14,4 +15,7 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
 	r.HandleFunc("/", pc.Home).Methods("GET").Name("home")
 	r.HandleFunc("/about", pc.About).Methods("GET").Name("about")
+
+	// 中间件：强制内容类型为 HTML
+	r.Use(middlewares.ForceHTML)
 }
